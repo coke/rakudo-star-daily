4b28297f3983761394fc047c28dd6c9e1c205cb4
Will "Coke" Coleda <will@coleda.com>
2014-05-19 13:41:26 +0000

    today (automated commit)

--- bin/star.sh
+++ bin/star.sh
@@ -4,13 +4,15 @@
 PATH=/usr/local/bin:$PATH
 
 # save current directory
+TOP_DIR=`pwd`
 LOG_DIR=`pwd`/log
 REPO_DIR=`pwd`/repos
 
 # start fresh
-rm -rf star
 
-for impl in "parrot"; do
+for impl in MoarVM parrot; do
+    cd $TOP_DIR;
+    rm -rf star-$impl
     git clone $REPO_DIR/star.git star-$impl
     cd star-$impl
 
@@ -18,30 +20,32 @@ for impl in "parrot"; do
 
     git clone $REPO_DIR/parrot.git
     (cd parrot && git describe --tags > VERSION_GIT)
+    git clone $REPO_DIR/MoarVM.git
+    (cd MoarVM && git describe --tags > VERSION_GIT)
     git clone $REPO_DIR/nqp.git
     (cd nqp && git ls-files > MANIFEST; git describe > VERSION)
     git clone $REPO_DIR/rakudo.git
     (cd rakudo && git ls-files > MANIFEST; git describe > VERSION)
 
     # setup the modules to pull the latest, not just the declared version)
-    git submodule init
-    git submodule update
-    (cd modules; for file in * ; do (cd $file && git fetch origin; git reset --hard origin/master); done)
+    #git submodule init
+    #git submodule update
+    #(cd modules; for file in * ; do (cd $file && git fetch origin; git reset --hard origin/master); done)
 
     make -f tools/star/Makefile manifest
 
     # get submodules # ??? is this doing anything?
-    git submodule foreach git pull origin master 2>&1 | tee submodule.log
+    #git submodule foreach git pull origin master 2>&1 | tee submodule.log
 
     # log the versions used on everything
-    echo "parrot" > $LOG_DIR/$impl-version.log
-    cat parrot/VERSION_GIT >> $LOG_DIR/$impl-version.log
+    echo $impl > $LOG_DIR/$impl-version.log
+    cat $impl/VERSION_GIT >> $LOG_DIR/$impl-version.log
     echo "Rakudo" > $LOG_DIR/$impl-version.log
     cat rakudo/VERSION >> $LOG_DIR/$impl-version.log
     echo "NQP"   >> $LOG_DIR/$impl-version.log
     cat nqp/VERSION >> $LOG_DIR/$impl-version.log
     echo "modules"  >> $LOG_DIR/$impl-version.log
-    (cd modules; for file in * ; do echo "--------"; echo $file; (cd $file; git log HEAD^..HEAD); done) >> $LOG_DIR/$impl-version.log
+    #(cd modules; for file in * ; do echo "--------"; echo $file; (cd $file; git log HEAD^..HEAD); done) >> $LOG_DIR/$impl-version.log
     
     # make a release candidate
     make -f tools/star/Makefile release VERSION=daily 2>&1 | tee makefile.log
@@ -51,7 +55,14 @@ for impl in "parrot"; do
     
     # build it.
     cd rakudo-star-daily
-    perl Configure.pl --backend=parrot --gen-nqp --gen-parrot 2>&1 | tee $LOG_DIR/$impl-configure.log
+    config_args = "";
+    if [ "$impl" = "parrot" ] ; then 
+        config_args = "--gen-parrot"
+    elif [ "$impl" = "MoarVM" ] ; then
+        config_args = "--gen-moar"
+    fi
+    perl Configure.pl --backend=$impl --gen-nqp $config_args 2>&1 | tee $LOG_DIR/$impl-configure.log
+
     make install 2>&1 | tee $LOG_DIR/$impl-build.log
     
     # run tests
